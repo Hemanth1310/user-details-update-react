4db6b39e03661075c4932fb38bfbba7de0ce7003
hemanth1310 <hemanth13k@gmail.com>
2020-10-15 22:11:25 +0530

    secound-commit

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import classes from './Main.module.css'
 import './Main.module.css'
 import Login from '../Login/Login'
-// import Tabs from '../../components/Tabs/Tabs'
-import NavigationItems from './navigationItems/NavigationItems'
+
 import Userdata from '../UserData/UserDetails'
 export class Main extends Component {
    
@@ -54,17 +53,6 @@ export class Main extends Component {
         return (
            
             <div className={classes.ContactData}> 
-
-    
-                {/* <div className={classes.head}>
-               <ul className={classes.NavigationItems}>
-               <NavigationItems link='/login' >Login Info</NavigationItems> 
-                <NavigationItems link='/user-details' >User Info</NavigationItems>
-               </ul>
-               </div>
-                <br></br> */}
-
-{/* <p style={{font:"AR BARKLEY"}}>Pick One</p> */}
 
                 <div className={classes.head}>
                     <div className={oneClasses.join(' ')} onClick={this.tabOneClickHAndle}>Account Settings</div>
